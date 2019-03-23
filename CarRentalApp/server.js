23c3488bf525b70080ea5a9acb17c751a6d04318
Harmangrewal8 <46337286+Harmangrewal8@users.noreply.github.com>
2019-03-23 13:27:59 -0700

    functionality added
    calculates time period
    passing multiple variable to ejs
    complex queries

--- CarRentalApp/server.js
+++ CarRentalApp/server.js
@@ -6,13 +6,23 @@ var app = express();
 const bodyparser = require('body-parser');
 var session = require('express-session');
 
+
+
 app.set("view engine", "ejs");
+
 app.use(bodyparser.urlencoded());
 app.use(session({
 	secret: 'secret',
 	resave: true,
 	saveUninitialized: true
 }));
+
+app.use(function(req, res, next) {
+    res.locals.status = req.session.loggedin;
+    res.locals.user = req.session.username;
+    res.locals.msg = req.session.msg;
+    next();
+  });
 
 //mysql connection
 var mysqlConnection = mysql.createConnection({
@@ -33,9 +43,7 @@ mysqlConnection.connect((err) => {
 
 app.listen(3000, () => console.log('Express server is running at port no : 3000'));
 
-    app.use(express.static('public'))
-
-
+ app.use(express.static('public'))
 
 //user registration    
 app.get('/register', (req, res) => {
@@ -105,41 +113,71 @@ app.get('/result', (req, res) => {
 });
 
 var userLocation;
-
+var diffDays;
+var weeks;
+var dayrem;
+var hours;
 app.post('/result', (req, res) => {
     userLocation = req.body.userlocation;
     var fromDate = (req.body.fromDate);
     var fromTime = req.body.fromTime;
-    var toDate = req.body.toDate;
+    var toDate = (req.body.toDate);
     var toTime = req.body.toTime;
-    var types = req.body.types;
+ 
     console.log(fromDate+" "+fromTime);
     console.log(toDate+" "+toTime);
+    
+//calculating days
+
+    var firstDate = new Date(fromDate.replace('-' , '/'));
+    var secondDate = new Date(toDate.replace('-' , '/'));    
+    diffDays = Math.abs((firstDate.getTime() - secondDate.getTime()) / 86400000);
+   
+
+//calculating hours and mins   
+        fromTime = fromTime.split(":");
+        toTime = toTime.split(":");
+        var startDate = new Date(0, 0, 0, fromTime[0], fromTime[1], 0);
+        var endDate = new Date(0, 0, 0, toTime[0], toTime[1], 0);
+        var diff = endDate.getTime() - startDate.getTime();
+        hours = Math.floor(diff / 1000 / 60 / 60);
+        diff = hours * 1000 * 60 * 60;
+        var minutes = Math.floor(diff / 1000 / 60);
+
+        if (hours < 0)
+           {hours = hours + 24;}
+    else{
+        
+
+        console.log(("days: "+ diffDays+" hours: "+ hours +" minutes: "+ minutes));
+
+        //finally calculating weeks and remaining days and hours
+        weeks = Math.floor(diffDays/7);
+        dayrem = diffDays % 7;
+
+        }
     
 
     console.log("User: "+req.session.username);
     console.log("logged in: "+req.session.loggedin);
-
-if(req.session.loggedin == true){
-    mysqlConnection.query('SELECT * FROM car WHERE location = ?', [userLocation], (err, rows, fields) => {
-        if (!err){
-            if(res == null){
-                res.send("oops");
+    if(req.session.loggedin == true){
+        mysqlConnection.query('SELECT car.carType, name, brandName, carStatus, location, rates.weekly, rates.hourly, rates.daily, insurance.weeklyIn, insurance.hourlyIn, insurance.dailyIn from car inner join rates on car.carType = rates.carType inner join insurance on car.carType = insurance.carType WHERE location = ? ', [userLocation], (err, rows, fields) => {
+            if (!err){
+                if(res == null){
+                    res.send("oops");
+                }
+                else {
+                     res.render("cars", { data: rows , weeks: weeks, dayrem: dayrem, hours: hours});
+                }
             }
-            else {
-                 res.render("cars", { data: rows });
-            }
-        }
-           
-        else
-            console.log(err);
-    })
-
-}
-  else{
-      res.send("Please Log in or Sign Up");
-      
-  }  
+               
+            else
+                console.log(err);
+        })
+    }
+    else{
+        res.redirect('/login');
+    }
    
     
 });
@@ -155,13 +193,13 @@ app.post('/type', (req, res) => {
     console.log(userLocation);
     var types = req.body.types;
     if(types == null){
-        mysqlConnection.query('SELECT * FROM car WHERE location = ?', [userLocation], (err, rows, fields) => {
+        mysqlConnection.query('SELECT car.carType, name, brandName, carStatus, location, rates.weekly, rates.hourly, rates.daily, insurance.weeklyIn, insurance.hourlyIn, insurance.dailyIn from car inner join rates on car.carType = rates.carType left join insurance on car.carType = insurance.carType WHERE location = ? ', [userLocation], (err, rows, fields) => {
             if (!err){
                 if(res == null){
                     res.send("oops");
                 }
                 else {
-                     res.render("cars", { data: rows });
+                    res.render("cars", { data: rows , weeks: weeks, dayrem: dayrem, hours: hours});
                 }
             }
                
@@ -177,7 +215,7 @@ app.post('/type', (req, res) => {
                 }
                 else {
                     
-                     res.render("cars", { data: rows });
+                    res.render("cars", { data: rows , weeks: weeks, dayrem: dayrem, hours: hours});
                 }
             }
                
@@ -188,6 +226,17 @@ app.post('/type', (req, res) => {
     }
     
     
+});
+
+//reserve car
+app.get("/reserve", (req, res)=>{
+
+});
+app.post("/reserve", (req, res)=>{
+    var cost = req.body.cost;
+    res.send(cost);
+
+
 });
 
 //button routes
